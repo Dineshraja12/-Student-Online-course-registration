f8cc5bc4b69de9b3a60c4f7645191b4e0b5ba93b
DINESH RAJA R <rdineshraja2004@gmail.com>
2025-11-22 17:38:35 +0530

    Remove unused methods from Student class
    Removed getter and toString methods from Student class.

--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,17 @@
+import jakarta.persistence.*;
+
+@Entity
 public class Student {
+
+    @Id
     private String studentId;
+
     private String name;
+
+    public Student() {}
 
     public Student(String studentId, String name) {
         this.studentId = studentId;
         this.name = name;
-    }
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    @Override
-    public String toString() {
-        return studentId + " - " + name;
     }
 }
