2a484cef53b5812d486808fa647d53bee3f43729
DINESH RAJA R <rdineshraja2004@gmail.com>
2025-11-22 17:40:23 +0530

    Integrate Hibernate for course and student registration
    Refactor registration process to use Hibernate for database operations.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,45 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        // Add sample courses
+        session.save(new Course("C101", "Java Programming", 2));
+        session.save(new Course("C102", "Python Basics", 3));
+        session.save(new Course("C103", "Web Development", 1));
+
+        tx.commit();
+
         Scanner sc = new Scanner(System.in);
-        RegistrationSystem system = new RegistrationSystem();
-
-        system.addCourse(new Course("C101", "Java Programming", 2));
-        system.addCourse(new Course("C102", "Python Basics", 3));
-        system.addCourse(new Course("C103", "Web Development", 1));
-
         System.out.println("Enter Student ID: ");
-        String id = sc.nextLine();
+        String sid = sc.nextLine();
 
-        System.out.println("Enter Student Name: ");
+        System.out.println("Enter Name: ");
         String name = sc.nextLine();
 
-        Student s = new Student(id, name);
+        session.beginTransaction();
+        session.save(new Student(sid, name));
+        session.getTransaction().commit();
 
-        System.out.println("\nAvailable Courses:");
-        system.showCourses();
-
-        System.out.println("\nEnter Course ID to Register: ");
+        System.out.println("Enter Course ID to Register: ");
         String cid = sc.nextLine();
 
-        system.register(s, cid);
+        session.beginTransaction();
+        Course c = session.get(Course.class, cid);
+
+        if (c != null && c.register()) {
+            session.update(c);
+            session.save(new Registration(sid, cid));
+            System.out.println("Registered Successfully!");
+        } else {
+            System.out.println("No seats / Invalid course!");
+        }
+
+        session.getTransaction().commit();
+        session.close();
     }
 }
